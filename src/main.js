5fbf717493fc61d6b2a277783b09333053b7b400
thtrangg <nguyenthutrang01bn@gmail.com>
2022-04-09 00:37:50 +0700

    install tailwind + header

--- src/main.js
+++ src/main.js
@@ -3,14 +3,25 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "./assets/tailwind.css";
+
+//import bootstrap 4
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+
+//import fontawesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+
+library.add(faSpinner);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 new Vue({
   router,
