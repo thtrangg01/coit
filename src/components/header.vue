955bff2de957d36cb5537dcd5f576b55707fcf3c
Bui Cuong <cuongbuilh@gmail.com>
2022-04-08 09:55:51 +0700

    turn off eslint

---
+++ src/components/header.vue
@@ -0,0 +1,12 @@
+/* eslint-disable */
+<template lang="">
+  <div>header</div>
+</template>
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  name: "header",
+});
+</script>
+<style lang=""></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+/* eslint-disable */ 
+<template lang="">
+  <div>
+    <headerVue />
+    <div>home content</div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+import headerVue from "@/components/header.vue";
+
+export default defineComponent({
+  name: "HomeView",
+  components: {headerVue}
+})
+</script>
+<style lang="">
+  
+</style>
