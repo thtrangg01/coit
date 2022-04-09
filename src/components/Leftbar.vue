d0b17b4c5fc7e439321d1479013b5b94b18c95e8
Bui Cuong <cuongbuilh@gmail.com>
2022-04-09 16:11:04 +0700

    Update Leftbar.vue

--- src/components/Leftbar.vue
+++ src/components/Leftbar.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
-    <router-link to="/" class="router-link">  <p>Trang chủ</p></router-link>
-    <br/>
+    <router-link to="/" class="router-link"> <p>Trang chủ</p></router-link>
     <router-link to="/new">Chuyện của tôi</router-link>
+    <div>
+      <p v-b-toggle.collapse-1 variant="primary">
+        Toggle Collapse <i class="fas fa-angle-down"></i>
+      </p>
+      <b-collapse id="collapse-1" class="mt-2">
+        <p class="card-text"><span class="bold">Hanoi:</span> 0123456789</p>
+      </b-collapse>
+    </div>
   </div>
 </template>
 
@@ -12,7 +19,8 @@ export default {
 };
 </script>
 
-
 <style scoped>
-  
+.bold {
+  font-weight: bold;
+}
 </style>
