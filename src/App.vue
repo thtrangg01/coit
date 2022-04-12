7ed466b3ba17e9871f49df3d63c9ede0ed791dd8
thtrangg <thutrangnguyen01bn@gmail.com>
2022-04-12 19:14:56 +0700

    fix conflict

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <h class="mb-10" />
-
-    <!-- <PostSTT class="mb-10 mt-10" />
-    <NFItem class="mt-10" /> -->
     <Statistical />
 
     <f class="mt-10" />
@@ -11,14 +7,13 @@
 </template>
 
 <script>
-import h from "./components/header.vue";
 import Statistical from "./components/Statistical.vue";
 import NFItem from "./components/NFItem.vue";
 import PostSTT from "./components/CreatePostComponent.vue";
 import f from "./components/footer.vue";
 export default {
   name: "App",
-  components: { h, NFItem, PostSTT, f, Statistical },
+  components: { NFItem, PostSTT, f, Statistical },
 };
 </script>
 <style></style>
