0a0b985fba8bb680a7b2f12fa495afe3bc211989
Huyen <tkhuyen1212@gmail.com>
2022-04-13 11:46:33 +0700

    left-bar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
     <h class="mb-10" />
-
-    <!-- <PostSTT class="mb-10 mt-10" />
-    <NFItem class="mt-10" /> -->
+    <LeftBar class="absolute" />
+    
     <Statistical />
+    <PostSTT class="mb-10 mt-10" />
+    <NFItem class="mt-10" />
 
     <f class="mt-10" />
   </div>
@@ -14,12 +15,13 @@
 import h from "./components/header.vue";
 import Statistical from "./components/Statistical.vue";
 import NFItem from "./components/NFItem.vue";
+import LeftBar from "./components/LeftBar.vue";
 import PostSTT from "./components/CreatePostComponent.vue";
 import f from "./components/footer.vue";
 export default {
   name: "App",
 
-  components: { h, NFItem, PostSTT, f, Statistical },
+  components: { h, NFItem, PostSTT, f, Statistical, LeftBar },
 };
 </script>
 <style></style>
