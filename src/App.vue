c52a61f61603a6833cda88c3e6e1215a9d8640e1
Bui Cuong <cuongbuilh@gmail.com>
2022-04-13 18:40:46 +0700

    add route

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,18 @@
 <template>
   <div id="app">
     <h class="mb-10" />
-<<<<<<< tkhuyen
-    <!-- <PostSTT class="mb-10 mt-10" />
-    <NFItem class="mt-10" /> -->
-    <Statistical />
-=======
-    <PostSTT class="mb-10 mt-10" />
-    <NFItem class="mt-10" />
->>>>>>> master
-    <f class="mt-10" />
+    <leftBar class='absolute'/>
+    <router-view/>
   </div>
 </template>
 
 <script>
 import h from "./components/header.vue";
-import Statistical from "./components/Statistical.vue";
-import NFItem from "./components/NFItem.vue";
-import PostSTT from "./components/CreatePostComponent.vue";
-import f from "./components/footer.vue";
+import leftBar from "./components/LeftBar.vue";
+
 export default {
   name: "App",
-<<<<<<< tkhuyen
-  components: { h, NFItem, PostSTT, f, Statistical },
-=======
-  components: { h, NFItem, PostSTT, f },
->>>>>>> master
+  components: { h, leftBar},
 };
 </script>
 <style></style>
