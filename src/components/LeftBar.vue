fd4d60053c412a3f040c7738ca953a7f8db8310e
Trinh Khanh Huyen <tkhuyen1212@gmail.com>
2022-04-14 17:51:02 +0700

    fix left bar

--- src/components/LeftBar.vue
+++ src/components/LeftBar.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="ml-24">
     <router-link to="/">
-      <p class="cursor-pointer text-black no-underline">
+      <p class="cursor-pointer text-black no-underline text-lg">
         Trang chủ
       </p></router-link
     >
     <router-link to="/feeds"
-      ><p class="cursor-pointer mb-2 text-black no-underline">
+      ><p class="cursor-pointer mb-2 text-black no-underline text-lg">
         Chuyện của tôi
       </p></router-link
     >
 
     <div class="mt-3">
       <!-- Using modifiers -->
-      <button v-b-toggle.collapse-2>Hotlines</button>
+      <button class="text-lg" v-b-toggle.collapse-2>Hotlines</button>
 
       <b-collapse id="collapse-2">
-        <p class="ml-4 w-60 wrap">
+        <p class="ml-4 w-60 wrap mt-2">
           - Bộ Y tế: 19003228 <br />
           - Sở Y tế Hà Nội: 19009095 <br />
           - Sở Thông tin và Truyền thông Hà Nội: 1022
