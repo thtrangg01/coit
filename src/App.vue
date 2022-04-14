05bac38fbe26c4317a37de69d97de92e54266c53
admin <tuanan8601@gmail.com>
2022-04-14 15:46:56 +0700

    An fix notice and notice item

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 
 <script>
 import h from "./components/header.vue";
-import LeftBar from "./components/CreatePostComponent.vue";
+import LeftBar from "./components/LeftBar.vue";
 import Footer from "./components/footer.vue";
 
 export default {
